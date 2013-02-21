00ff8eabfa75ff7c0bff66c54f6cba23f35a1c4f
Tatu Saloranta <tsaloranta@gmail.com>
2013-02-21 14:13:45 -0800

    Add an experimental "nuevo" encoder, for testing improvement ideas

---
+++ src/main/java/com/ning/compress/lzf/nuevo/UnsafeChunkEncoder.java
@@ -0,0 +1,452 @@
+/* Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package com.ning.compress.lzf.nuevo;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import com.ning.compress.BufferRecycler;
+import com.ning.compress.lzf.LZFChunk;
+
+/**
+ * Class that handles actual encoding of individual chunks.
+ * Resulting chunks can be compressed or non-compressed; compression
+ * is only used if it actually reduces chunk size (including overhead
+ * of additional header bytes)
+ * 
+ * @author Tatu Saloranta (tatu.saloranta@iki.fi)
+ */
+public final class UnsafeChunkEncoder
+{
+    // Beyond certain point we won't be able to compress; let's use 16 bytes as cut-off
+    private static final int MIN_BLOCK_TO_COMPRESS = 16;
+
+    private static final int MIN_HASH_SIZE = 256;
+
+    // Not much point in bigger tables, with 8k window
+    private static final int MAX_HASH_SIZE = 16384;
+
+    private static final int MAX_OFF = 1 << 13; // 8k
+    private static final int MAX_REF = (1 << 8) + (1 << 3); // 264
+    
+    // // Encoding tables etc
+
+    private final BufferRecycler _recycler;
+
+    /**
+     * Hash table contains lookup based on 3-byte sequence; key is hash
+     * of such triplet, value is offset in buffer.
+     */
+    private int[] _hashTable;
+    
+    private final int _hashModulo;
+
+    /**
+     * Buffer in which encoded content is stored during processing
+     */
+    private byte[] _encodeBuffer;
+
+    /**
+     * Small buffer passed to LZFChunk, needed for writing chunk header
+     */
+    private byte[] _headerBuffer;
+    
+    /**
+     * @param totalLength Total encoded length; used for calculating size
+     *   of hash table to use
+     */
+    public UnsafeChunkEncoder(int totalLength)
+    {
+        // Need room for at most a single full chunk
+        int largestChunkLen = Math.min(totalLength, LZFChunk.MAX_CHUNK_LEN);       
+        int suggestedHashLen = calcHashLen(largestChunkLen);
+        _recycler = BufferRecycler.instance();
+        _hashTable = _recycler.allocEncodingHash(suggestedHashLen);
+        _hashModulo = _hashTable.length - 1;
+        // Ok, then, what's the worst case output buffer length?
+        // length indicator for each 32 literals, so:
+        // 21-Feb-2013, tatu: Plus we want to prepend chunk header in place:
+        int bufferLen = largestChunkLen + ((largestChunkLen + 31) >> 5) + LZFChunk.MAX_HEADER_LEN;
+        _encodeBuffer = _recycler.allocEncodingBuffer(bufferLen);
+    }
+
+    /**
+     * Alternate constructor used when we want to avoid allocation encoding
+     * buffer, in cases where caller wants full control over allocations.
+     */
+    private UnsafeChunkEncoder(int totalLength, boolean bogus)
+    {
+        int largestChunkLen = Math.max(totalLength, LZFChunk.MAX_CHUNK_LEN);
+        int suggestedHashLen = calcHashLen(largestChunkLen);
+        _recycler = BufferRecycler.instance();
+        _hashTable = _recycler.allocEncodingHash(suggestedHashLen);
+        _hashModulo = _hashTable.length - 1;
+        _encodeBuffer = null;
+    }
+
+    public static UnsafeChunkEncoder nonAllocatingEncoder(int totalLength) {
+        return new UnsafeChunkEncoder(totalLength, true);
+    }
+    
+    /*
+    ///////////////////////////////////////////////////////////////////////
+    // Public API
+    ///////////////////////////////////////////////////////////////////////
+     */
+    
+    /**
+     * Method to close once encoder is no longer in use. Note: after calling
+     * this method, further calls to {@link #encodeChunk} will fail
+     */
+    public void close()
+    {
+        byte[] buf = _encodeBuffer;
+        if (buf != null) {
+            _encodeBuffer = null;
+            _recycler.releaseEncodeBuffer(buf);
+        }
+        int[] ibuf = _hashTable;
+        if (ibuf != null) {
+            _hashTable = null;
+            _recycler.releaseEncodingHash(ibuf);
+        }
+    }
+    
+    /**
+     * Method for compressing (or not) individual chunks
+     */
+    public LZFChunk encodeChunk(byte[] data, int offset, int len)
+    {
+        if (len >= MIN_BLOCK_TO_COMPRESS) {
+            /* If we have non-trivial block, and can compress it by at least
+             * 2 bytes (since header is 2 bytes longer), let's compress:
+             */
+            int compLen = tryCompress(data, offset, offset+len, _encodeBuffer, 0);
+            if (compLen < (len-2)) { // nah; just return uncompressed
+                return LZFChunk.createCompressed(len, _encodeBuffer, 0, compLen);
+            }
+        }
+        // Otherwise leave uncompressed:
+        return LZFChunk.createNonCompressed(data, offset, len);
+    }
+
+    /**
+     * Alternate chunk compression method that will append encoded chunk in
+     * pre-allocated buffer. Note that caller must ensure that the buffer is
+     * large enough to hold not just encoded result but also intermediate
+     * result; latter may be up to 4% larger than input; caller may use
+     * {@link UnsafeLZFEncoder#estimateMaxWorkspaceSize(int)} to calculate
+     * necessary buffer size.
+     * 
+     * @return Offset in output buffer after appending the encoded chunk
+     * 
+     * @since 0.9.7
+     */
+    public int appendEncodedChunk(final byte[] input, final int inputPtr, final int inputLen,
+            final byte[] outputBuffer, final int outputPos)
+    {
+        if (inputLen >= MIN_BLOCK_TO_COMPRESS) {
+            /* If we have non-trivial block, and can compress it by at least
+             * 2 bytes (since header is 2 bytes longer), use as-is
+             */
+            final int compStart = outputPos + LZFChunk.HEADER_LEN_COMPRESSED;
+            final int end = tryCompress(input, inputPtr, inputPtr+inputLen, outputBuffer, compStart);
+            final int uncompEnd = (outputPos + LZFChunk.HEADER_LEN_NOT_COMPRESSED) + inputLen;
+            if (end < uncompEnd) { // yes, compressed by at least one byte
+                final int compLen = end - compStart;
+                LZFChunk.appendCompressedHeader(inputLen, compLen, outputBuffer, outputPos);
+                return end;
+            }
+        }
+        // Otherwise append as non-compressed chunk instead (length + 5):
+        return LZFChunk.appendNonCompressed(input, inputPtr, inputLen, outputBuffer, outputPos);
+    }
+    
+    /**
+     * Method for encoding individual chunk, writing it to given output stream.
+     */
+    public void encodeAndWriteChunk(byte[] data, int offset, int len, OutputStream out)
+        throws IOException
+    {
+        // Pre-0.9.7
+        /*
+        byte[] headerBuf = _headerBuffer;
+        if (headerBuf == null) {
+            _headerBuffer = headerBuf = new byte[LZFChunk.MAX_HEADER_LEN];
+        }
+        if (len >= MIN_BLOCK_TO_COMPRESS) {
+            // If we have non-trivial block, and can compress it by at least
+            // 2 bytes (since header is 2 bytes longer), let's compress:
+            int compLen = tryCompress(data, offset, offset+len, _encodeBuffer, 0);
+            if (compLen < (len-2)) { // yes, compressed block is smaller (consider header is 2 bytes longer)
+                LZFChunk.writeCompressedHeader(len, compLen, out, headerBuf);
+                out.write(_encodeBuffer, 0, compLen);
+                return;
+            }
+        }
+        // Otherwise leave uncompressed:
+        LZFChunk.writeNonCompressedHeader(len, out, headerBuf);
+        out.write(data, offset, len);
+        */
+        
+        if (len >= MIN_BLOCK_TO_COMPRESS) {
+            // If we have non-trivial block, and can compress it by at least
+            // 2 bytes (since header is 2 bytes longer), let's compress:
+            int compEnd = tryCompress(data, offset, offset+len, _encodeBuffer, LZFChunk.HEADER_LEN_COMPRESSED);
+            final int compLen = compEnd - LZFChunk.HEADER_LEN_COMPRESSED;
+            if (compLen < (len-2)) { // yes, compressed block is smaller (consider header is 2 bytes longer)
+                LZFChunk.appendCompressedHeader(len, compLen, _encodeBuffer, 0);
+                out.write(_encodeBuffer, 0, compEnd);
+                return;
+            }
+        }
+        // Otherwise leave uncompressed:
+        byte[] headerBuf = _headerBuffer;
+        if (headerBuf == null) {
+            _headerBuffer = headerBuf = new byte[LZFChunk.MAX_HEADER_LEN];
+        }
+        LZFChunk.writeNonCompressedHeader(len, out, headerBuf);
+        out.write(data, offset, len);
+    }
+
+    /**
+     * Main workhorse method that will try to compress given chunk, and return
+     * end position (offset to byte after last included byte)
+     * 
+     * @return Output pointer after handling content, such that <code>result - originalOutPost</code>
+     *    is the actual length of compressed chunk (without header)
+     */
+    protected int tryCompress(byte[] in, int inPos, int inEnd, byte[] out, int outPos)
+    {
+        final int[] hashTable = _hashTable;
+        ++outPos;
+        int seen = first(in, 0); // past 4 bytes we have seen... (last one is LSB)
+        int literals = 0;
+        inEnd -= 4;
+        final int firstPos = inPos; // so that we won't have back references across block boundary
+        
+        while (inPos < inEnd) {
+            byte p2 = in[inPos + 2];
+            // next
+            seen = (seen << 8) + (p2 & 255);
+            int off = hash(seen);
+            int ref = hashTable[off];
+            hashTable[off] = inPos;
+  
+            // First expected common case: no back-ref (for whatever reason)
+            if (ref >= inPos // can't refer forward (i.e. leftovers)
+                    || ref < firstPos // or to previous block
+                    || (off = inPos - ref) > MAX_OFF
+                    || in[ref+2] != p2 // must match hash
+                    || in[ref+1] != (byte) (seen >> 8)
+                    || in[ref] != (byte) (seen >> 16)) {
+                out[outPos++] = in[inPos++];
+                literals++;
+                if (literals == LZFChunk.MAX_LITERAL) {
+                    out[outPos - 33] = (byte) 31; // <= out[outPos - literals - 1] = MAX_LITERAL_MINUS_1;
+                    literals = 0;
+                    outPos++;
+                }
+                continue;
+            }
+            // match
+            int maxLen = inEnd - inPos + 2;
+            if (maxLen > MAX_REF) {
+                maxLen = MAX_REF;
+            }
+            if (literals == 0) {
+                outPos--;
+            } else {
+                out[outPos - literals - 1] = (byte) (literals - 1);
+                literals = 0;
+            }
+            int len = 3;
+            // find match length
+            while (len < maxLen && in[ref + len] == in[inPos + len]) {
+                len++;
+            }
+            len -= 2;
+            --off; // was off by one earlier
+            if (len < 7) {
+                out[outPos++] = (byte) ((off >> 8) + (len << 5));
+            } else {
+                out[outPos++] = (byte) ((off >> 8) + (7 << 5));
+                out[outPos++] = (byte) (len - 7);
+            }
+            out[outPos++] = (byte) off;
+            outPos++;
+            inPos += len;
+            seen = first(in, inPos);
+            seen = (seen << 8) + (in[inPos + 2] & 255);
+            hashTable[hash(seen)] = inPos;
+            ++inPos;
+            seen = (seen << 8) + (in[inPos + 2] & 255); // hash = next(hash, in, inPos);
+            hashTable[hash(seen)] = inPos;
+            ++inPos;
+        }
+        // try offlining the tail
+        return handleTail(in, inPos, inEnd+4, out, outPos, literals);
+    }
+
+    private final int handleTail(byte[] in, int inPos, int inEnd, byte[] out, int outPos,
+            int literals)
+    {
+        while (inPos < inEnd) {
+            out[outPos++] = in[inPos++];
+            literals++;
+            if (literals == LZFChunk.MAX_LITERAL) {
+                out[outPos - literals - 1] = (byte) (literals - 1);
+                literals = 0;
+                outPos++;
+            }
+        }
+        out[outPos - literals - 1] = (byte) (literals - 1);
+        if (literals == 0) {
+            outPos--;
+        }
+        return outPos;
+    }
+
+    /*
+    ///////////////////////////////////////////////////////////////////////
+    // Internal methods
+    ///////////////////////////////////////////////////////////////////////
+     */
+    
+    private static int calcHashLen(int chunkSize)
+    {
+        // in general try get hash table size of 2x input size
+        chunkSize += chunkSize;
+        // but no larger than max size:
+        if (chunkSize >= MAX_HASH_SIZE) {
+            return MAX_HASH_SIZE;
+        }
+        // otherwise just need to round up to nearest 2x
+        int hashLen = MIN_HASH_SIZE;
+        while (hashLen < chunkSize) {
+            hashLen += hashLen;
+        }
+        return hashLen;
+    }
+
+    private final int first(byte[] in, int inPos) {
+        return (in[inPos] << 8) + (in[inPos + 1] & 0xFF);
+    }
+
+    /*
+    private static int next(int v, byte[] in, int inPos) {
+        return (v << 8) + (in[inPos + 2] & 255);
+    }
+*/
+
+    private final int hash(int h) {
+        // or 184117; but this seems to give better hashing?
+        return ((h * 57321) >> 9) & _hashModulo;
+        // original lzf-c.c used this:
+        //return (((h ^ (h << 5)) >> (24 - HLOG) - h*5) & _hashModulo;
+        // but that didn't seem to provide better matches
+    }
+    
+    /*
+    ///////////////////////////////////////////////////////////////////////
+    // Alternative experimental version using Unsafe
+    // NOTE: not currently used, retained for future inspiration...
+    ///////////////////////////////////////////////////////////////////////
+     */
+
+    /*
+    private static final Unsafe unsafe;
+    static {
+        try {
+            Field theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
+            theUnsafe.setAccessible(true);
+            unsafe = (Unsafe) theUnsafe.get(null);
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static final long BYTE_ARRAY_OFFSET = unsafe.arrayBaseOffset(byte[].class);
+
+    private final int MASK = 0xFFFFFF;
+    
+    private final int get3Bytes(byte[] src, int srcIndex)
+    {
+        return unsafe.getInt(src, BYTE_ARRAY_OFFSET + srcIndex) & MASK;
+    }
+
+    private int tryCompress(byte[] in, int inPos, int inEnd, byte[] out, int outPos)
+    {
+        final int[] hashTable = _hashTable;
+        ++outPos;
+        int literals = 0;
+        inEnd -= 4;
+        final int firstPos = inPos; // so that we won't have back references across block boundary
+        
+        while (inPos < inEnd) {
+            int seen = get3Bytes(in, inPos);
+            int off = hash(seen);
+            int ref = hashTable[off];
+            hashTable[off] = inPos;
+  
+            // First expected common case: no back-ref (for whatever reason)
+            if (ref >= inPos // can't refer forward (i.e. leftovers)
+                    || ref < firstPos // or to previous block
+                    || (off = inPos - ref) > MAX_OFF
+                    || get3Bytes(in, ref) != seen
+                    ) {
+                out[outPos++] = in[inPos++];
+                literals++;
+                if (literals == LZFChunk.MAX_LITERAL) {
+                    out[outPos - 33] = (byte) 31; // <= out[outPos - literals - 1] = MAX_LITERAL_MINUS_1;
+                    literals = 0;
+                    outPos++;
+                }
+                continue;
+            }
+            // match
+            int maxLen = inEnd - inPos + 2;
+            if (maxLen > MAX_REF) {
+                maxLen = MAX_REF;
+            }
+            if (literals == 0) {
+                outPos--;
+            } else {
+                out[outPos - literals - 1] = (byte) (literals - 1);
+                literals = 0;
+            }
+            int len = 3;
+            while (len < maxLen && in[ref + len] == in[inPos + len]) {
+                len++;
+            }
+            len -= 2;
+            --off; // was off by one earlier
+            if (len < 7) {
+                out[outPos++] = (byte) ((off >> 8) + (len << 5));
+            } else {
+                out[outPos++] = (byte) ((off >> 8) + (7 << 5));
+                out[outPos++] = (byte) (len - 7);
+            }
+            out[outPos] = (byte) off;
+            outPos += 2;
+            inPos += len;
+            hashTable[hash(get3Bytes(in, inPos))] = inPos;
+            ++inPos;
+            hashTable[hash(get3Bytes(in, inPos))] = inPos;
+            ++inPos;
+        }
+        // try offlining the tail
+        return handleTail(in, inPos, inEnd+4, out, outPos, literals);
+    }
+    */
+}
--- src/test/java/perf/ManualPerfComparison.java
+++ src/test/java/perf/ManualPerfComparison.java
@@ -3,6 +3,7 @@ package perf;
 import java.io.*;
 
 import com.ning.compress.lzf.*;
+import com.ning.compress.lzf.nuevo.UnsafeLZFEncoder;
 import com.ning.compress.lzf.util.ChunkDecoderFactory;
 
 /**
@@ -11,24 +12,28 @@ import com.ning.compress.lzf.util.ChunkDecoderFactory;
  */
 public class ManualPerfComparison
 {
-    private int size = 0;
+    protected int size = 0;
  
-    private byte[] _lzfEncoded;
+    protected byte[] _lzfEncoded;
     
     private void test(byte[] input) throws Exception
     {
         _lzfEncoded = LZFEncoder.encode(input);
-        
-//        int i = 0;
+
         // Let's try to guestimate suitable size... to get to 10 megs to process
         final int REPS = (int) ((double) (10 * 1000 * 1000) / (double) input.length);
 
+        final int TYPES = 2;
+        final int WARMUP_ROUNDS = 5;
+        int i = 0;
+        int roundsDone = 0;
+        final long[] times = new long[TYPES];
+        
         System.out.println("Read "+input.length+" bytes to compress, uncompress; will do "+REPS+" repetitions");
 
         while (true) {
             try {  Thread.sleep(100L); } catch (InterruptedException ie) { }
-//            int round = (i++ % 4);
-            int round = 1;
+            int round = (i++ % TYPES);
 
             String msg;
             boolean lf = (round == 0);
@@ -42,6 +47,11 @@ public class ManualPerfComparison
                 msecs = testLZFCompress(REPS, input);
                 break;
             case 1:
+                msg = "LZF-Unsafe compress/block";
+                msecs = testLZFUnsafeCompress(REPS, input);
+                break;
+                /*
+            case 1:
                 msg = "LZF compress/stream";
                 msecs = testLZFCompressStream(REPS, input);
                 break;
@@ -53,18 +63,40 @@ public class ManualPerfComparison
                 msg = "LZF decompress/stream";
                 msecs = testLZFDecompressStream(REPS, _lzfEncoded);
                 break;
+                */
             default:
                 throw new Error();
             }
             
+            // skip first 5 rounds to let results stabilize
+            if (roundsDone >= WARMUP_ROUNDS) {
+                times[round] += msecs;
+            }
+            System.out.printf("Test '%s' [%d bytes] -> %d msecs\n", msg, size, msecs);
+            if (lf) {
+                ++roundsDone;
+                if ((roundsDone % 3) == 0 && roundsDone > WARMUP_ROUNDS) {
+                    double den = (double) (roundsDone - WARMUP_ROUNDS);
+                    System.out.printf("Averages after %d rounds (pre / no): %.1f / %.1f msecs\n",
+                            (int) den,
+                            times[0] / den, times[1] / den);
+                            
+                }
+                System.out.println();
+            }
+            if ((i % 17) == 0) {
+                System.out.println("[GC]");
+                Thread.sleep(100L);
+                System.gc();
+                Thread.sleep(100L);
+            }
             if (lf) {
                 System.out.println();
             }
-            System.out.println("Test '"+msg+"' ["+size+" bytes] -> "+msecs+" msecs");
         }
     }
 
-    private final long testLZFCompress(int REPS, byte[] input) throws Exception
+    protected final long testLZFCompress(int REPS, byte[] input) throws Exception
     {
         long start = System.currentTimeMillis();
         byte[] comp = null;
@@ -75,7 +107,18 @@ public class ManualPerfComparison
         return System.currentTimeMillis() - start;
     }
 
-    private final long testLZFCompressStream(int REPS, byte[] input) throws Exception
+    protected final long testLZFUnsafeCompress(int REPS, byte[] input) throws Exception
+    {
+        long start = System.currentTimeMillis();
+        byte[] comp = null;
+        while (--REPS >= 0) {
+            comp = UnsafeLZFEncoder.encode(input);
+        }
+        size = comp.length;
+        return System.currentTimeMillis() - start;
+    }
+    
+    protected final long testLZFCompressStream(int REPS, byte[] input) throws Exception
     {
         long start = System.currentTimeMillis();
         while (--REPS >= 0) {
@@ -88,7 +131,7 @@ public class ManualPerfComparison
         return System.currentTimeMillis() - start;
     }
     
-    private final long testLZFDecompress(int REPS, byte[] encoded) throws Exception
+    protected final long testLZFDecompress(int REPS, byte[] encoded) throws Exception
     {
         size = encoded.length;
         long start = System.currentTimeMillis();
@@ -103,7 +146,7 @@ public class ManualPerfComparison
         return System.currentTimeMillis() - start;
     }
 
-    private final long testLZFDecompressStream(int REPS, byte[] encoded) throws Exception
+    protected final long testLZFDecompressStream(int REPS, byte[] encoded) throws Exception
     {
         final byte[] buffer = new byte[8000];
         size = 0;
