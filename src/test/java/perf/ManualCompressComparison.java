f64fbf69ca13a960be2d40a706c8d7edf5d6f573
Tatu Saloranta <tsaloranta@gmail.com>
2013-03-08 21:58:08 -0800

    refactor perf test to try to do better comparisons

--- src/test/java/perf/ManualCompressComparison.java
+++ src/test/java/perf/ManualCompressComparison.java
@@ -11,51 +11,53 @@ import com.ning.compress.lzf.*;
 public class ManualCompressComparison
 {
     protected int size = 0;
- 
-    protected byte[] _lzfEncoded;
     
-    private void test(byte[] input) throws Exception
+    private void test(String[] names, byte[][] docs, int totalSize) throws Exception
     {
-        _lzfEncoded = LZFEncoder.encode(input);
+        final int DOC_COUNT = docs.length;
 
         // Let's try to guestimate suitable size... to get to 10 megs to process
-        final int REPS = Math.max(1, (int) ((double) (10 * 1000 * 1000) / (double) input.length));
+        // but, with more docs, give more time
+        final int REPS = Math.max(1, (int) ((double) (10 * 1000 * 1000 * Math.sqrt(DOC_COUNT)) / (double) totalSize));
 
 //        final int TYPES = 1;
         final int TYPES = 2;
         final int WARMUP_ROUNDS = 5;
-        int i = 0;
+        int roundTotal = 0;
         int roundsDone = 0;
-        final long[] times = new long[TYPES];
+        final long[][] times = new long[DOC_COUNT][];
+        for (int i = 0; i < times.length; ++i) {
+            times[i] = new long[TYPES];
+        }
         
-        System.out.println("Read "+input.length+" bytes to compress, uncompress; will do "+REPS+" repetitions");
+        System.out.println("Read "+totalSize+" bytes to compress, uncompress; will do "+REPS+" repetitions");
 
         // But first, validate!
-        _preValidate(input);
-        
-        while (true) {
+        _preValidate(docs);
+        int[] msecs = new int[DOC_COUNT];
+        boolean roundDone = false;
+
+        for (;; ++roundTotal) {
             try {  Thread.sleep(100L); } catch (InterruptedException ie) { }
-            int round = (i++ % TYPES);
-
+            int round = (roundTotal % TYPES);
             String msg;
-            boolean lf = (round == 0);
-
-            long msecs;
+            long msec;
             
             switch (round) {
 
             case 0:
                 msg = "LZF-Unsafe compress/block";
-                msecs = testLZFUnsafeCompress(REPS, input);
+                msec = testLZFUnsafeCompress(REPS, docs, msecs);
                 break;
             case 1:
                 msg = "LZF compress/block";
-                msecs = testLZFSafeCompress(REPS, input);
+                msec = testLZFSafeCompress(REPS, docs, msecs);
+                roundDone = true;
                 break;
                 /*
             case 1:
                 msg = "LZF compress/stream";
-                msecs = testLZFCompressStream(REPS, input);
+                msec = testLZFCompressStream(REPS, _lzfEncoded, msecs);
                 break;
                 */
             default:
@@ -64,25 +66,19 @@ public class ManualCompressComparison
             
             // skip first 5 rounds to let results stabilize
             if (roundsDone >= WARMUP_ROUNDS) {
-                times[round] += msecs;
-            }
-            System.out.printf("Test '%s' [%d bytes] -> %d msecs\n", msg, size, msecs);
-            if (lf) {
-                ++roundsDone;
-                if ((roundsDone % 3) == 0 && roundsDone > WARMUP_ROUNDS) {
-                    double den = (double) (roundsDone - WARMUP_ROUNDS);
-                    if (times.length == 1) {
-                        System.out.printf("Averages after %d rounds: %.1f msecs\n",
-                                (int) den, times[0] / den);
-                    } else {
-                        System.out.printf("Averages after %d rounds (NEW / old): %.1f / %.1f msecs\n",
-                                (int) den,
-                                times[0] / den, times[1] / den);
-                    }
-                    System.out.println();
+                for (int i = 0; i < DOC_COUNT; ++i) {
+                    times[i][round] += msecs[i];
                 }
             }
-            if ((i % 17) == 0) {
+            System.out.printf("Test '%s' [%d bytes] -> %d msecs\n", msg, size, msec);
+            if (roundDone) {
+                roundDone = false;
+                ++roundsDone;
+                if ((roundsDone % 3) == 0 && roundsDone > WARMUP_ROUNDS) {
+                    _printResults((roundsDone - WARMUP_ROUNDS), names, times);
+                }
+            }
+            if ((roundTotal % 17) == 0) {
                 System.out.println("[GC]");
                 Thread.sleep(100L);
                 System.gc();
@@ -91,92 +87,158 @@ public class ManualCompressComparison
         }
     }
 
-    protected void _preValidate(byte[] input) throws LZFException
+    protected void _printResults(int rounds, String[] names, long[][] timeSets)
     {
-        byte[] encoded1 = LZFEncoder.encode(input);
-        byte[] encoded2 = LZFEncoder.safeEncode(input);
-
-        if (encoded1.length == encoded2.length) {
-            for (int i = 0, len = encoded1.length; i < len; ++i) {
-                if (encoded1[i] != encoded2[i]) {
-                    throw new IllegalStateException("Compressed contents differ at "+i+"/"+len);
+        System.out.printf("Averages after %d rounds:", rounds);
+        double den = (double) rounds;
+        double[] totals = null;
+        for (int file = 0; file < names.length; ++file) {
+            System.out.printf(" %s(", names[file]);
+            long[] times = timeSets[file];
+            if (totals == null) {
+                totals = new double[times.length];
+            }
+            for (int i = 0; i < times.length; ++i){
+                if (i > 0) {
+                    System.out.print('/');
+                }
+                double msecs = times[i] / den;
+                System.out.printf("%.1f", msecs);
+                totals[i] += msecs;
+            }
+            System.out.printf(")");
+        }
+        System.out.println();
+        // then totals
+        System.out.printf("  for a total of: ");
+        for (int i = 0; i < totals.length; ++i){
+            if (i > 0) {
+                System.out.print('/');
+            }
+            double msecs = totals[i];
+            System.out.printf("%.1f", msecs);
+        }
+        System.out.println();
+    }
+    
+    protected void _preValidate(byte[][] inputs) throws LZFException
+    {
+        int index = 0;
+        for (byte[] input : inputs) {
+            ++index;
+            byte[] encoded1 = LZFEncoder.encode(input);
+            byte[] encoded2 = LZFEncoder.safeEncode(input);
+    
+            if (encoded1.length == encoded2.length) {
+                for (int i = 0, len = encoded1.length; i < len; ++i) {
+                    if (encoded1[i] != encoded2[i]) {
+                        throw new IllegalStateException("Compressed contents of entry "+index+"/"+input.length+" differ at "+i+"/"+len);
+                    }
+                }
+            } else {
+                // Actually, let's allow some slack...
+                int diff = Math.abs(encoded1.length - encoded2.length);
+                // 1/256 seems fine (but at least 16)
+                int maxDiff = Math.max(16, encoded1.length >> 8);
+                if (diff > maxDiff) {
+                   throw new IllegalStateException("Compressed contents of entry "+index+"/"+input.length+" differ by more than "+maxDiff+" bytes: expected "+encoded1.length+", got "+encoded2.length);
+                }
+                System.err.printf("WARN: sizes differ slightly, %d vs %s (old/new)\n", encoded1.length, encoded2.length);
+            } 
+            // uncompress too
+            byte[] output1 = LZFDecoder.decode(encoded1);
+            byte[] output2 = LZFDecoder.decode(encoded2);
+            if (output1.length != output2.length) {
+                throw new IllegalStateException("Uncompressed contents of entry "+index+"/"+input.length+" differ!");
+            }
+            for (int i = 0, len = output1.length; i < len; ++i) {
+                if (output1[i] != output2[i]) {
+                    throw new IllegalStateException("Uncompressed contents of entry "+index+"/"+input.length+" differ at "+i+"/"+len);
                 }
             }
-        } else {
-            // Actually, let's allow some slack...
-            int diff = Math.abs(encoded1.length - encoded2.length);
-            // 1/256 seems fine (but at least 16)
-            int maxDiff = Math.max(16, encoded1.length >> 8);
-            if (diff > maxDiff) {
-               throw new IllegalStateException("Compressed contents differ by more than "+maxDiff+" bytes: expected "+encoded1.length+", got "+encoded2.length);
-            }
-            System.err.printf("WARN: sizes differ slightly, %d vs %s (old/new)\n", encoded1.length, encoded2.length);
-        } 
-        // uncompress too
-        byte[] output1 = LZFDecoder.decode(encoded1);
-        byte[] output2 = LZFDecoder.decode(encoded2);
-        if (output1.length != output2.length) {
-            throw new IllegalStateException("Uncompressed contents differ!");
-        }
-        for (int i = 0, len = output1.length; i < len; ++i) {
-            if (output1[i] != output2[i]) {
-                throw new IllegalStateException("Uncompressed contents differ at "+i+"/"+len);
-            }
         }
     }
     
-    protected final long testLZFSafeCompress(int REPS, byte[] input) throws Exception
+    protected final long testLZFSafeCompress(int REPS, byte[][] inputs, int[] msecs) throws Exception
     {
-        long start = System.currentTimeMillis();
-        byte[] comp = null;
-        while (--REPS >= 0) {
-            comp = LZFEncoder.safeEncode(input);
+        size = 0;
+        final long mainStart = System.currentTimeMillis();
+        for (int i = 0, len = inputs.length; i < len; ++i) {
+            final long start = System.currentTimeMillis();
+            byte[] comp = null;
+            int reps = REPS;
+            while (--reps >= 0) {
+                comp = LZFEncoder.safeEncode(inputs[i]);
+            }
+            size += comp.length;
+            msecs[i] = (int) (System.currentTimeMillis() - start);
         }
-        size = comp.length;
-        return System.currentTimeMillis() - start;
+        return System.currentTimeMillis() - mainStart;
     }
 
-    protected final long testLZFUnsafeCompress(int REPS, byte[] input) throws Exception
+    protected final long testLZFUnsafeCompress(int REPS, byte[][] inputs, int[] msecs) throws Exception
     {
-        long start = System.currentTimeMillis();
-        byte[] comp = null;
-        while (--REPS >= 0) {
-            comp = LZFEncoder.encode(input);
+        size = 0;
+        final long mainStart = System.currentTimeMillis();
+        for (int i = 0, len = inputs.length; i < len; ++i) {
+            final long start = System.currentTimeMillis();
+            byte[] comp = null;
+            int reps = REPS;
+            while (--reps >= 0) {
+                comp = LZFEncoder.encode(inputs[i]);
+            }
+            size += comp.length;
+            msecs[i] = (int) (System.currentTimeMillis() - start);
         }
-        size = comp.length;
-        return System.currentTimeMillis() - start;
+        return System.currentTimeMillis() - mainStart;
     }
     
-    protected final long testLZFCompressStream(int REPS, byte[] input) throws Exception
+    protected final long testLZFCompressStream(int REPS, byte[][] inputs, int[] msecs) throws Exception
     {
-        long start = System.currentTimeMillis();
-        while (--REPS >= 0) {
+        size = 0;
+        final long mainStart = System.currentTimeMillis();
+        for (int i = 0, len = inputs.length; i < len; ++i) {
+            final long start = System.currentTimeMillis();
             BogusOutputStream bogus = new BogusOutputStream();
-            LZFOutputStream out = new LZFOutputStream(bogus);
-            out.write(input);
-            out.close();
-            size = bogus.length();
+
+            int reps = REPS;
+            while (--reps >= 0) {
+                bogus.reset();
+                LZFOutputStream out = new LZFOutputStream(bogus);
+                out.write(inputs[i]);
+                out.close();
+            }                
+            size += bogus.length();
+            msecs[i] = (int) (System.currentTimeMillis() - start);
         }
-        return System.currentTimeMillis() - start;
+        return System.currentTimeMillis() - mainStart;
     }
     
     public static void main(String[] args) throws Exception
     {
-        if (args.length != 1) {
-            System.err.println("Usage: java ... [file]");
+        if (args.length < 1) {
+            System.err.println("Usage: java ... [file1] ... [fileN]");
             System.exit(1);
         }
-        File f = new File(args[0]);
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream((int) f.length());
-        byte[] buffer = new byte[4000];
-        int count;
-        FileInputStream in = new FileInputStream(f);
-        
-        while ((count = in.read(buffer)) > 0) {
-            bytes.write(buffer, 0, count);
+        byte[][] data = new byte[args.length][];
+        String[] names = new String[args.length];
+        int totalSize = 0;
+        for (int i = 0; i < args.length; ++i) {
+            File f = new File(args[i]);
+            names[i] = f.getName();
+            ByteArrayOutputStream bytes = new ByteArrayOutputStream((int) f.length());
+            byte[] buffer = new byte[4000];
+            int count;
+            FileInputStream in = new FileInputStream(f);
+            
+            while ((count = in.read(buffer)) > 0) {
+                bytes.write(buffer, 0, count);
+            }
+            in.close();
+            data[i] = bytes.toByteArray();
+            totalSize += data[i].length;
         }
-        in.close();
-        new ManualCompressComparison().test(bytes.toByteArray());
+        new ManualCompressComparison().test(names, data, totalSize);
     }
 
     private final static class BogusOutputStream extends OutputStream
@@ -194,6 +256,8 @@ public class ManualCompressComparison
         }
 
         public int length() { return _bytes; }
+
+        public void reset() { _bytes = 0; }
     }
 
 }
