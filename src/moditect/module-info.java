175ff33bff1c031a09371ef921f2f969882568d6
Tatu Saloranta <tatu.saloranta@iki.fi>
2021-01-06 21:22:51 -0800

    Fix #49, add module-info (JPMS)

---
+++ src/moditect/module-info.java
@@ -0,0 +1,12 @@
+// Hand-crafted 06-Jan-2021 by tatu.saloranta@iki.fi
+module com.ning.compress.lzf {
+    requires transitive java.xml;
+
+    exports com.ning.compress;
+    exports com.ning.compress.gzip;
+    exports com.ning.compress.lzf;
+    // Not sure if this needs to be exported but...
+    exports com.ning.compress.lzf.impl;
+    exports com.ning.compress.lzf.parallel;
+    exports com.ning.compress.lzf.util;
+}
